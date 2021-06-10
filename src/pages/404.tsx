07cfa3e299e3cd75ca288926dec6e414677d3da2
netoudi <netoudi@rocketmail.com>
2021-06-10 20:51:19 -0300

    Add custom page 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import { Flex, Heading, Text } from '@chakra-ui/react';
+import Header from '../components/Header';
+
+export default function Custom404() {
+  return (
+    <>
+      <Header />
+      <Flex
+        w="100%"
+        h="60vh"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+      >
+        <Heading color="gray.600" fontSize={['26px', '36px']} fontWeight="500">
+          Ops!
+        </Heading>
+        <Text color="gray.600" fontSize={['18px', '28px']} fontWeight="300">
+          Página não encontrada,{' '}
+          <strong>
+            <Link href="/">voltar para home.</Link>
+          </strong>
+        </Text>
+      </Flex>
+    </>
+  );
+}
